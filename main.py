e4b1dcb82e11fb58cb60faac9c86620e3cbf2d99
gbatistela <giulianobatistela@gmail.com>
2024-01-21 18:23:51 -0300

    first

--- main.py
+++ main.py
@@ -4,26 +4,27 @@ app = FastAPI()
 
 
 # Obtenemos el dataframe
-
-df = pd.read_csv("bq_query1.csv")
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+df = pd.read_csv("bq_query1.csv")
+
+
+
 @app.get("/Modelo_Machine_Learning.")
-def train_regression_model_with_feature(feature, label, test_size=0.2, random_state=42):
+def train_regression_model_with_feature(feature, label):
 
     # Selección de características (X) y etiquetas (y)
     X = df[[feature]]
     y = df[label]
-
+    
     # División del conjunto de datos en entrenamiento y prueba
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Crear y entrenar el modelo de regresión
-    regression_model = RandomForestRegressor(random_state=random_state)
+    regression_model = RandomForestRegressor(random_state=42)
     regression_model.fit(X_train, y_train)
 
     # Hacer predicciones en el conjunto de prueba
